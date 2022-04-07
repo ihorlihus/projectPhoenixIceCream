0bb5b77b8da5f0ded3174da861833a1b9531f4b8
IgorMishchenko <e-imishchen@outlook.com>
2022-04-07 17:13:59 +0300

    modal_03

---
+++ src/header-modal-window.js
@@ -0,0 +1,15 @@
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[header-modal-window-open]"),
+    closeModalBtn: document.querySelector("[header-modal-window-close]"),
+    modal: document.querySelector("[header-modal-window]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
